be60d4758c3ca7e4e5a6a5ee5d04ba7eff897f60
Denis <plax1denis@mail.ru>
2024-07-12 18:42:50 +0300

    update ci 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.serialization") version "2.0.0"
 }
 
-val maxApkSizeValue = 20
+val maxApkSizeValue = 10
 val validationEnabledValue = true
 val analysisEnabledValue = true
 tgReporter {
